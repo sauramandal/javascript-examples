e0b2274c017a2247bda31da9bd4e720c2511a8d1
Saura Mandal <saura.mandal1@gmail.com>
2021-06-29 21:21:11 +0530

    Add toTitleCase function

--- src/object-operations.js
+++ src/object-operations.js
@@ -17,3 +17,9 @@ export const deepCompare = (obj1, obj2) => {
   }
   return true;
 };
+
+export function toTitleCase(str) {
+  return str.replace(/\w\S*/g, (txt) => {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
